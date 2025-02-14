3c2c4d9a9c0029245bdde0f5a16327010e856388
Mike Khaw <mikekhawnow@gmail.com>
2025-02-13 18:43:33 -0800

    fonts

---
+++ src/app/fonts/font.ts
@@ -0,0 +1,16 @@
+import { Inter, Roboto } from "next/font/google";
+
+//when loading a variable font, you don't need to specify the font weight
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+// this is not a variable font, so we need to specify the font weight
+export const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { Inter, Space_Grotesk } from "next/font/google";
+
+// This is a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ["latin"] });
+
+//load space grotesk
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 const page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center flex-col gap-4">
-      <h1>Hello World</h1>
+      <h1 className="text-4xl font-bold tracking-tight">Hello World (inter)</h1>
+      <h1 className="text-4xl font-roboto font-bold tracking-normal">
+        Hello World (roboto)
+      </h1>
       <Button>Click me</Button>
     </div>
   );
