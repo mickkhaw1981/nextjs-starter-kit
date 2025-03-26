2e305bfce762bca34cbadab573273e0008348ebe
Mike Khaw <mikekhawnow@gmail.com>
2025-03-26 06:54:40 -0700

    saving work

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import forms from "@tailwindcss/forms";
 import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 import plugin from "tailwindcss/plugin";
 
 const config = {
@@ -58,9 +57,6 @@ const config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
